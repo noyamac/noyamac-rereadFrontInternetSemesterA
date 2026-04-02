eb46256fb7ffd86a11387d65d21a0872a43056ec
noya <noyam1003@gmail.com>
2026-04-02 13:48:49 +0300

    deleted unused files

--- reread_front/src/App.tsx
+++ reread_front/src/App.tsx
@@ -1,21 +1,17 @@
 import { lazy, Suspense } from 'react';
-import './App.css';
+import { Spinner } from 'react-bootstrap';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import NavigationBar from './shared/components/navbar/navbar';
-import { Spinner } from 'react-bootstrap';
 
 const HomePage = lazy(() => import('./features/home/home'));
-// const Profile = lazy(() => import('./pages/Profile'));
-// const PostDetail = lazy(() => import('./pages/PostDetail'));
-// const Upload = lazy(() => import('./pages/Upload'));
 
 function App() {
-return (
+  return (
     <Router>
-      <NavigationBar /> 
-      
+      <NavigationBar />
+
       <main className="bg-light min-vh-100">
-        <Suspense 
+        <Suspense
           fallback={
             <div className="d-flex justify-content-center pt-5">
               <Spinner animation="border" variant="primary" />
@@ -24,8 +20,6 @@ return (
         >
           <Routes>
             <Route path="/" element={<HomePage />} />
-            {/* <Route path="/profile" element={<Profile />} /> */}
-            {/* <Route path="/post/:id" element={<PostDetail />} /> */}
           </Routes>
         </Suspense>
       </main>
